492f335c8463f8139ec34f3d0b25823ef7541383
Bright Onapito <onabright@gmail.com>
2021-07-13 15:31:58 +0300

    chore: update connection string

--- database.php
+++ database.php
@@ -1,4 +1,4 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=crud_test', 'root', 'B0nd21');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=crud_test', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
